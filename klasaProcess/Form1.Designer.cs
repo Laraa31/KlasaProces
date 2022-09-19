b2c65b394df8245772c0488c9209bf8e97d46c26
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-09-19 11:56:15 +0200

    Add files via upload

---
+++ klasaProcess/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace klasaProcess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIExplorer = new System.Windows.Forms.Button();
+            this.btnWord = new System.Windows.Forms.Button();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.btnStartInfo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIExplorer
+            // 
+            this.btnIExplorer.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIExplorer.Location = new System.Drawing.Point(12, 20);
+            this.btnIExplorer.Name = "btnIExplorer";
+            this.btnIExplorer.Size = new System.Drawing.Size(152, 99);
+            this.btnIExplorer.TabIndex = 0;
+            this.btnIExplorer.Text = "Pokrenite Internet Explorer";
+            this.btnIExplorer.UseVisualStyleBackColor = true;
+            this.btnIExplorer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnWord
+            // 
+            this.btnWord.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWord.Location = new System.Drawing.Point(219, 21);
+            this.btnWord.Name = "btnWord";
+            this.btnWord.Size = new System.Drawing.Size(152, 99);
+            this.btnWord.TabIndex = 1;
+            this.btnWord.Text = "Pokrenite Word";
+            this.btnWord.UseVisualStyleBackColor = true;
+            this.btnWord.Click += new System.EventHandler(this.btnWord_Click);
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcel.Location = new System.Drawing.Point(12, 126);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(152, 99);
+            this.btnExcel.TabIndex = 2;
+            this.btnExcel.Text = "Pokrenite Excel";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // btnStartInfo
+            // 
+            this.btnStartInfo.Font = new System.Drawing.Font("Myanmar Text", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartInfo.Location = new System.Drawing.Point(219, 126);
+            this.btnStartInfo.Name = "btnStartInfo";
+            this.btnStartInfo.Size = new System.Drawing.Size(152, 99);
+            this.btnStartInfo.TabIndex = 3;
+            this.btnStartInfo.Text = "StartInfo";
+            this.btnStartInfo.UseVisualStyleBackColor = true;
+            this.btnStartInfo.Click += new System.EventHandler(this.btnStartInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 247);
+            this.Controls.Add(this.btnStartInfo);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.btnWord);
+            this.Controls.Add(this.btnIExplorer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIExplorer;
+        private System.Windows.Forms.Button btnWord;
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.Button btnStartInfo;
+    }
+}
+
---
+++ klasaProcess/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace klasaProcess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("IExplore.exe");
+        }
+
+        private void btnWord_Click(object sender, EventArgs e)
+        {
+            Process.Start("WINWORD.EXE");
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            Process.Start("Excel.exe");
+        }
+
+        private void btnStartInfo_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("IExplore.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Normal;
+            startInfo.Arguments = "http://web2.ss-zcrnje-rovinj.skole.hr/";
+            Process.Start(startInfo);
+        }
+    }
+}
